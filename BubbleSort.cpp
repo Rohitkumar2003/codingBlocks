95992e7d717d179000dc0a17e7c39f4dd9ef0330
Rohit Kumar <Mr.developer2003@gmail.com>
2024-08-10 13:24:49 +0530

    code complete

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -8,5 +8,26 @@ int main(int argc, char const *argv[]){
     
 //  Bubble Sort 
 
+    int n;
+    cin >>n;
+
+    int arr[n];
+
+    for(int i=0; i<n;i++) {
+            cin>>arr[i];
+    }
+    
+    for(int i = 1; i<=n-1;i++) {
+        for(int j=0; j<n-1; j++) {
+            if(arr[j]>arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+        for(int j=0; j<n;j++) {
+            cout<<arr[j]<<" ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
